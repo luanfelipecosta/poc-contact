11282475954bd11d3674c2a1158cb229f18cf8c8
Luan Costa <luanfelipecosta@gmail.com>
2021-03-16 19:35:13 -0300

    first

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <>
+      <h1>Contato</h1>
+      <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+    </>
+  );
+}
