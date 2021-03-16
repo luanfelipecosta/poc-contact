30f77d0d73a3529cc24cff43b48de56c5ae8a475
Luan Costa <luanfelipecosta@gmail.com>
2021-03-16 19:47:33 -0300

    fix

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <>
       <h1>Contato</h1>
-      <img src="/vercel.svg" alt="Vercel Logo"/>
+      <img src="/v2/vercel.svg" alt="Vercel Logo"/>
     </>
   );
 }
